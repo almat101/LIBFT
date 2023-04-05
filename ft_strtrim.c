b5070c0d2798f36465313519be6254d31b12adef
Alessandro Matta <amatta@e3r1p1.42roma.it>
2023-04-05 17:32:33 +0200

    daily backup

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: amatta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 12:36:52 by amatta            #+#    #+#             */
-/*   Updated: 2023/04/05 12:52:20 by amatta           ###   ########.fr       */
+/*   Updated: 2023/04/05 17:31:04 by amatta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 #include <stdio.h>
 #include <string.h>
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	s1len;
-	size_t	lset;
+	char	*ps1;
+	size_t	start;
+	size_t	end;
+	size_t	len;
 
-	s1len = strlen(s1);
-	lset = strlen(set);
-	while (*s1)
-	{
-		if (
-
+	start = 0;
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	while (s1[start] && ft_strchr(set, s1[start]))
+			start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	len = end - start;
+	ps1 = ft_substr(s1, start, len + 1);
+	return (ps1);
 }
